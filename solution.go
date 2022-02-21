4061c97dd4b7da30b84dff39b0688cbe5ea0a1f0
RavshanovUsmonbek <usmonravshanov@list.ru>
2022-02-21 10:39:44 +0500

    triangle calculation fixed

--- solution.go
+++ solution.go
@@ -12,23 +12,20 @@ import "math"
 
 type sideType uint64
 
-// const (
-// 	SidesCircle sideType = iota
-// 	_
-// 	_
-// 	SidesTriangle
-// 	SidesSquare
-// )
-const SidesCircle sideType = 0
-const SidesTriangle sideType = 3
-const SidesSquare sideType = 4
+const (
+	SidesCircle sideType = iota
+	_
+	_
+	SidesTriangle
+	SidesSquare
+)
 
 func CalcSquare(sideLen float64, sidesNum sideType) float64 {
 	switch sidesNum {
 	case 0:
 		return math.Pi * math.Pow(sideLen, 2)
 	case 3:
-		return math.Sqrt(sideLen) * math.Pow(sideLen, 2) / 4
+		return math.Sqrt(3) * math.Pow(sideLen, 2) / 4
 	case 4:
 		return math.Pow(sideLen, 2)
 	default:
